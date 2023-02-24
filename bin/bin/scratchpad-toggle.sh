aedc2b186918381d99250626c1a8ea13d4b9c915
Alexander Neville <git@afneville.com>
2023-02-24 00:06:02 +0000

    correct node selection

--- bin/bin/scratchpad-toggle.sh
+++ bin/bin/scratchpad-toggle.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/bash
 
-if [ -z "$(xdotool search --class scratchpad | head -n 1)" ]
+pid=$(xdotool search --class scratchpad | head -n 1)
+if [ -z "$pid" ]
 then
     alacritty --class scratchpad -e zsh -ic "tmux new-session -As scratchpad" &
-    # pid=$(xdotool search --class scratchpad | head -n 1)
-    # while [[ -z "$pid" ]]; do
-    #     pid=$(xdotool search --class scratchpad | head -n 1)
-    # done
-    # bspc node $pid --flag hidden -f
+else
+    bspc node $pid --flag hidden -f
 fi
-
-pid=$(xdotool search --class scratchpad | head -n 1)
-bspc node $pid --flag hidden -f
