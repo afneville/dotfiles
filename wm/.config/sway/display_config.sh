f6dc6f011e9e1ae7404fd082aa7f46af546180ca
Alexander Neville <git@afneville.com>
2023-07-08 10:10:09 +0100

    new external display configuration

--- wm/.config/sway/display_config.sh
+++ wm/.config/sway/display_config.sh
@@ -2,10 +2,9 @@
 
 if [[ -n "$(swaymsg -t get_outputs | grep HDMI-A-1)" ]]; then
     swaymsg output eDP-1 disable
+    swaymsg output DP-2 scale 2 transform 90 anticlockwise position 0 0
+    swaymsg output HDMI-A-1 mode 3840x2160@60Hz scale 2 position 1080 480
     swaymsg workspace 1
-    if [[ -n "$(swaymsg -t get_outputs | grep 3840)" ]]; then
-        swaymsg output HDMI-A-1 resolution 3840x2160 scale 2
-    fi
 else
     swaymsg output eDP-1 enable
 fi
