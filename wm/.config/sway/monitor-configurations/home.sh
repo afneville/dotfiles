b37154e2ed43128b108381890d9460b72dc0fbc4
Alexander Neville <git@afneville.com>
2024-09-26 08:07:04 +0100

    update sway configuration

--- wm/.config/sway/monitor-configurations/home.sh
+++ wm/.config/sway/monitor-configurations/home.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-horiz="$(swaymsg --pretty -t get_outputs | grep ETN4P00759SL0 | cut -d' ' -f2)"
-vert="$(swaymsg --pretty -t get_outputs | grep ETGAM01835SL0 | cut -d' ' -f2)"
+vert="$(swaymsg --pretty -t get_outputs | grep ETN4P00759SL0 | cut -d' ' -f2)"
+horiz="$(swaymsg --pretty -t get_outputs | grep ETGAM01835SL0 | cut -d' ' -f2)"
 swaymsg output eDP-1 disable
 swaymsg output ${vert} transform 90 anticlockwise position 0 0
 swaymsg output ${horiz} position 2160 960
