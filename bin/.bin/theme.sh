8b81c0ebb545c52d1ec7f56f8866b37e871071e2
Alexander Neville <git@afneville.com>
2025-12-17 15:08:30 +0000

    change theme switcher

--- bin/.bin/theme.sh
+++ bin/.bin/theme.sh
@@ -8,21 +8,21 @@ if [ ! -d "${theme_dir}" ]; then
 fi
 
 if [ -n "$TMUX" ]; then
-    put_template() { printf '\033Ptmux;\033\033]4;%d;rgb:%s\033\033\\\033\\' $@; }
-    put_template_var() { printf '\033Ptmux;\033\033]%d;rgb:%s\033\033\\\033\\' $@; }
-    put_template_custom() { printf '\033Ptmux;\033\033]%s%s\033\033\\\033\\' $@; }
+    set_palette_color() { printf '\033Ptmux;\033\033]4;%d;rgb:%s\033\033\\\033\\' $@; }
+    set_term_param() { printf '\033Ptmux;\033\033]%d;rgb:%s\033\033\\\033\\' $@; }
+    send_escape_seq() { printf '\033Ptmux;\033\033]%s%s\033\033\\\033\\' $@; }
 elif [ "${TERM%%[-.]*}" = "screen" ]; then
-    put_template() { printf '\033P\033]4;%d;rgb:%s\007\033\\' $@; }
-    put_template_var() { printf '\033P\033]%d;rgb:%s\007\033\\' $@; }
-    put_template_custom() { printf '\033P\033]%s%s\007\033\\' $@; }
+    set_palette_color() { printf '\033P\033]4;%d;rgb:%s\007\033\\' $@; }
+    set_term_param() { printf '\033P\033]%d;rgb:%s\007\033\\' $@; }
+    send_escape_seq() { printf '\033P\033]%s%s\007\033\\' $@; }
 elif [ "${TERM%%-*}" = "linux" ]; then
-    put_template() { [ "$1" -lt 16 ] && printf "\e]P%x%s" "$1" "$(echo "$2" | sed 's/\///g')"; }
-    put_template_var() { true; }
-    put_template_custom() { true; }
+    set_palette_color() { [ "$1" -lt 16 ] && printf "\e]P%x%s" "$1" "$(echo "$2" | sed 's/\///g')"; }
+    set_term_param() { true; }
+    send_escape_seq() { true; }
 else
-    put_template() { printf '\033]4;%d;rgb:%s\033\\' $@; }
-    put_template_var() { printf '\033]%d;rgb:%s\033\\' $@; }
-    put_template_custom() { printf '\033]%s%s\033\\' $@; }
+    set_palette_color() { printf '\033]4;%d;rgb:%s\033\\' $@; }
+    set_term_param() { printf '\033]%d;rgb:%s\033\\' $@; }
+    send_escape_seq() { printf '\033]%s%s\033\\' $@; }
 fi
 
 add_slashes() {
@@ -30,13 +30,13 @@ add_slashes() {
     echo "${original:0:2}/${original:2:2}/${original:4:2}"
 }
 
-# pass arguments
 usage() {
     echo "Usage: $(basename "$0") [-t string] [-s]"
     echo "    -t name of the theme to set"
     echo "    -s set shell escape sequences only"
     exit 1
 }
+
 interactive=1
 shell_only=0
 while getopts "t:s" opt; do
@@ -74,33 +74,41 @@ else
 fi
 
 theme_path="${theme_dir}/${choice}.yaml"
-theme=()
-for c in base0{0..9} base0{A..F}; do
-    theme+=("$(sed -ne 's/'"${c}"': "\(.*\)".*/\1/p' "$theme_path")")
+
+# Parse YAML and export as environment variables
+for i in {0..9}; do
+    val=$(sed -ne 's/base0'"${i}"': "\(.*\)".*/\1/p' "$theme_path")
+    export "COL${i}=${val}"
+done
+
+for i in {10..15}; do
+    hex_char=$(printf "%X" $((i)))
+    val=$(sed -ne 's/base0'"${hex_char}"': "\(.*\)".*/\1/p' "$theme_path")
+    export "COL${i}=${val}"
 done
 
 # shell colours
-put_template 0 "$(add_slashes "${theme[0]}")"
-put_template 1 "$(add_slashes "${theme[8]}")"
-put_template 2 "$(add_slashes "${theme[11]}")"
-put_template 3 "$(add_slashes "${theme[10]}")"
-put_template 4 "$(add_slashes "${theme[13]}")"
-put_template 5 "$(add_slashes "${theme[14]}")"
-put_template 6 "$(add_slashes "${theme[12]}")"
-put_template 7 "$(add_slashes "${theme[5]}")"
+set_palette_color 0 "$(add_slashes "$COL0")"  # black
+set_palette_color 1 "$(add_slashes "$COL8")"  # red
+set_palette_color 2 "$(add_slashes "$COL11")" # green
+set_palette_color 3 "$(add_slashes "$COL10")" # yellow
+set_palette_color 4 "$(add_slashes "$COL13")" # blue
+set_palette_color 5 "$(add_slashes "$COL14")" # magenta
+set_palette_color 6 "$(add_slashes "$COL12")" # cyan
+set_palette_color 7 "$(add_slashes "$COL5")"  # white
 
-put_template 8  "$(add_slashes "${theme[3]}")"
-put_template 9  "$(add_slashes "${theme[8]}")"
-put_template 10 "$(add_slashes "${theme[11]}")"
-put_template 11 "$(add_slashes "${theme[10]}")"
-put_template 12 "$(add_slashes "${theme[13]}")"
-put_template 13 "$(add_slashes "${theme[14]}")"
-put_template 14 "$(add_slashes "${theme[12]}")"
-put_template 15 "$(add_slashes "${theme[5]}")"
+set_palette_color 8  "$(add_slashes "$COL3")"  # bright black
+set_palette_color 9  "$(add_slashes "$COL8")"  # bright red
+set_palette_color 10 "$(add_slashes "$COL11")" # bright green
+set_palette_color 11 "$(add_slashes "$COL10")" # bright yellow
+set_palette_color 12 "$(add_slashes "$COL13")" # bright blue
+set_palette_color 13 "$(add_slashes "$COL14")" # bright magenta
+set_palette_color 14 "$(add_slashes "$COL12")" # bright cyan
+set_palette_color 15 "$(add_slashes "$COL5")"  # bright white
 
-put_template_var 10 "$(add_slashes "${theme[5]}")"
-put_template_var 11 "$(add_slashes "${theme[0]}")"
-put_template_custom 12 ";7"
+set_term_param 10 "$(add_slashes "$COL5")"
+set_term_param 11 "$(add_slashes "$COL0")"
+send_escape_seq 12 ";7"
 
 if [ $shell_only -eq 1 ]; then
     exit # done
@@ -108,30 +116,25 @@ fi
 
 printf "#!/bin/sh\nshell_theme=\"%s\"" "$choice" >~/.sh_theme.sh
 
-[ -f "$HOME/.Xresources.conf.d/colours.in" ] && cp ~/.Xresources.conf.d/colours.in ~/.Xresources.conf.d/colours
-[ -f "$HOME/.config/alacritty/colours.toml.in" ] && cp ~/.config/alacritty/colours.toml.in ~/.config/alacritty/colours.toml.tmp
-[ -f "$HOME/.config/kitty/colours.conf.in" ] && cp ~/.config/kitty/colours.conf.in ~/.config/kitty/colours.conf
-[ -f "$HOME/.config/zathura/zathurarc.in" ] && cp ~/.config/zathura/zathurarc.in ~/.config/zathura/zathurarc
-[ -f "$HOME/.config/polybar/colours.ini.in" ] && cp ~/.config/polybar/colours.ini.in ~/.config/polybar/colours.ini
-[ -f "$HOME/.config/bspwm/bspwmrc.in" ] && cp ~/.config/bspwm/bspwmrc.in ~/.config/bspwm/bspwmrc
-[ -f "$HOME/.config/sway/colours.in" ] && cp ~/.config/sway/colours.in ~/.config/sway/colours
-[ -f "$HOME/.config/waybar/colours.css.in" ] && cp ~/.config/waybar/colours.css.in ~/.config/waybar/colours.css
-[ -f "$HOME/.config/swaylock/config.in" ] && cp ~/.config/swaylock/config.in ~/.config/swaylock/config
-[ -f "$HOME/.config/swaync/colours.css.in" ] && cp ~/.config/swaync/colours.css.in ~/.config/swaync/colours.css
-[ -f "$HOME/.config/nvim/colors/b16-inherit-from-shell.vim.in" ] && cp ~/.config/nvim/colors/b16-inherit-from-shell.vim.in ~/.config/nvim/colors/b16-inherit-from-shell.vim
+# Define template files
+template_files=(
+    "$HOME/.Xresources.conf.d/colours.in"
+    "$HOME/.config/alacritty/colours.toml.in"
+    "$HOME/.config/kitty/colours.conf.in"
+    "$HOME/.config/zathura/zathurarc.in"
+    "$HOME/.config/polybar/colours.ini.in"
+    "$HOME/.config/bspwm/bspwmrc.in"
+    "$HOME/.config/sway/colours.in"
+    "$HOME/.config/waybar/colours.css.in"
+    "$HOME/.config/swaylock/config.in"
+    "$HOME/.config/swaync/colours.css.in"
+    "$HOME/.config/nvim/colors/b16-inherit-from-shell.vim.in"
+)
 
-for i in {0..15}; do
-    [ -f "$HOME/.Xresources.conf.d/colours.in" ] && sed -i'.bak' -e "s/%col${i}%/${theme[$i]}/g" ~/.Xresources.conf.d/colours
-    [ -f "$HOME/.config/alacritty/colours.toml.in" ] && sed -i'.bak' -e "s/%col${i}%/${theme[$i]}/g" ~/.config/alacritty/colours.toml.tmp
-    [ -f "$HOME/.config/kitty/colours.conf.in" ] && sed -i'.bak' -e "s/%col${i}%/${theme[$i]}/g" ~/.config/kitty/colours.conf
-    [ -f "$HOME/.config/zathura/zathurarc.in" ] && sed -i'.bak' -e "s/%col${i}%/${theme[$i]}/g" ~/.config/zathura/zathurarc
-    [ -f "$HOME/.config/polybar/colours.ini.in" ] && sed -i'.bak' -e "s/%col${i}%/${theme[$i]}/g" ~/.config/polybar/colours.ini
-    [ -f "$HOME/.config/bspwm/bspwmrc.in" ] && sed -i'.bak' -e "s/%col${i}%/${theme[$i]}/g" ~/.config/bspwm/bspwmrc
-    [ -f "$HOME/.config/sway/colours.in" ] && sed -i'.bak' -e "s/%col${i}%/${theme[$i]}/g" ~/.config/sway/colours
-    [ -f "$HOME/.config/waybar/colours.css.in" ] && sed -i'.bak' -e "s/%col${i}%/${theme[$i]}/g" ~/.config/waybar/colours.css
-    [ -f "$HOME/.config/swaync/colours.css.in" ] && sed -i'.bak' -e "s/%col${i}%/${theme[$i]}/g" ~/.config/swaync/colours.css
-    [ -f "$HOME/.config/swaylock/config.in" ] && sed -i'.bak' -e "s/%col${i}%/${theme[$i]}/g" ~/.config/swaylock/config
-    [ -f "$HOME/.config/nvim/colors/b16-inherit-from-shell.vim.in" ] && sed -i'.bak' -e "s/%col${i}%/${theme[$i]}/g" ~/.config/nvim/colors/b16-inherit-from-shell.vim
+# Process each template file using envsubst
+for template in "${template_files[@]}"; do
+    if [ -f "$template" ]; then
+        output="${template%.in}"  # Remove .in suffix
+        envsubst < "$template" > "$output"
+    fi
 done
-
-mv ~/.config/alacritty/colours.toml.tmp ~/.config/alacritty/colours.toml
